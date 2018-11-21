b9942229d45b9e10ae00047babfc931165fd96c1
feynmansfedora <hdawg7797@yahoo.com>
2018-11-21 10:00:00 -0500

    Created helloworld.js, tutorial file

---
+++ js-lib/helloworld.js
@@ -0,0 +1,13 @@
+const ywot = require(./ywot.js); //Includes main library
+var client = new ywot.YWOT(); //Global websocket manager
+var main = ywot.openworld('testworld'); //This function is required to interface with specific worlds, in this case
+//https://www.yourworldoftext.com/testworld. Go check for the results there.
+var textspace = new ywot.Space(); //The main spatial manipulation object in ywot
+textspace.readfile('./helloworld.txt'); //Reads the local helloworld.txt file into the Space's data
+
+main.on('on', ()=>{ //Runs when the world websocket is initialized
+  let writedata = textspace.towrite(); //Generates a well-structured array of characters and positions based on internal data
+  main.write(writedata); //Pushes the write out to the yourworldoftext world
+});
+
+//Go check out the world now. It should be changed.
