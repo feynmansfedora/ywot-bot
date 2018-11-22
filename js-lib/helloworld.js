da753190f5fe048fb82a6a421824fa76b39f7966
feynmansfedora <hdawg7797@yahoo.com>
2018-11-22 12:38:45 -0500

    Included coordinates, fatal bug in helloworld.js

--- js-lib/helloworld.js
+++ js-lib/helloworld.js
@@ -6,7 +6,8 @@ var textspace = new ywot.Space(); //The main spatial manipulation object in ywot
 textspace.readfile('./helloworld.txt'); //Reads the local helloworld.txt file into the Space's data
 
 main.on('on', ()=>{ //Runs when the world websocket is initialized
-  let writedata = textspace.towrite(); //Generates a well-structured array of characters and positions based on internal data
+  let writedata = textspace.towrite(0,0); //Generates a well-structured array of characters and positions based on internal data
+  //                                ^Coordinates
   main.write(writedata); //Pushes the write out to the yourworldoftext world
 });
 
