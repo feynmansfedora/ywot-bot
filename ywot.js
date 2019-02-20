56005b7914c695ed9deb1c9e1af7db5df5f2ea61
Holden Rohrer <hdawg7797@yahoo.com>
2019-02-20 11:38:50 -0500

    Space.fill util

--- ywot.js
+++ ywot.js
@@ -353,6 +353,13 @@ function Space(){
       }
     }, x1, y1, x2, y2)
   }
+  this.fillchar = function(char, x=1, y=1){ //Fills space x,y with char
+    let row = Array(x*16).fill(char);
+    this.data = []; //Clears out Space
+    for (let i=0; i<y*8; i++){
+      this.data.push(row.splice());
+    }
+  }
 }
 exports.Space = Space
 
